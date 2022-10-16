7951f4b6fbb50831c64d41d0ac2d43221587c76c
jooijin <48429380+jooijin@users.noreply.github.com>
2022-10-16 16:39:11 +0900

    [22.10.16] 머스테치 화면 구성

--- src/main/java/com/jooijin/myJewerly/springboot/service/posts/PostsService.java
+++ src/main/java/com/jooijin/myJewerly/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.jooijin.myJewerly.springboot.service.posts;
 
 import com.jooijin.myJewerly.springboot.domain.posts.Posts;
 import com.jooijin.myJewerly.springboot.domain.posts.PostsRepository;
+import com.jooijin.myJewerly.springboot.web.dto.PostsListResponseDto;
 import com.jooijin.myJewerly.springboot.web.dto.PostsResponseDto;
 import com.jooijin.myJewerly.springboot.web.dto.PostsSaveRequestDto;
 import com.jooijin.myJewerly.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,6 +38,20 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
     }
 
 }
