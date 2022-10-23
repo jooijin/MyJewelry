7fad2125382583006c20b23445cf7ab3072d6922
jooijin <48429380+jooijin@users.noreply.github.com>
2022-10-24 01:01:37 +0900

    [22.10.24] 테스트 수정

--- src/test/java/com/jooijin/myJewerly/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/jooijin/myJewerly/springboot/domain/posts/PostsRepositoryTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDateTime;
@@ -14,7 +13,6 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
-@EnableJpaAuditing
 @SpringBootTest
 public class PostsRepositoryTest {
 
--- src/test/java/com/jooijin/myJewerly/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jooijin/myJewerly/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.jooijin.myJewerly.springboot.web;
 
+import com.jooijin.myJewerly.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -14,12 +18,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }
+)
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles = "USER")
     @Test
     public void returnHello() throws Exception {
         String hello = "hello";
@@ -29,6 +38,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles = "USER")
     @Test
     public void returnHelloDto() throws Exception {
         String name = "hello";
