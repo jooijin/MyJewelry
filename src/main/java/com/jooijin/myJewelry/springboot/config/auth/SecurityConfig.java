41b6cc137a6029cb14a678f8a8a5c0d879b91503
jooijin <48429380+jooijin@users.noreply.github.com>
2022-11-14 01:43:25 +0900

    [22.11.14] 이름 오타 수정^^;;

---
+++ src/main/java/com/jooijin/myJewelry/springboot/config/auth/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.jooijin.myJewelry.springboot.config.auth;
+
+import com.jooijin.myJewelry.springboot.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeHttpRequests()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .anyRequest().authenticated()
+                .and()
+                .logout().logoutSuccessUrl("/")
+                .and()
+                .oauth2Login().userInfoEndpoint().userService(customOAuth2UserService)
+                .and()
+                .and().build();
+    }
+}
---
+++ src/main/java/com/jooijin/myJewelry/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package com.jooijin.myJewelry.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
