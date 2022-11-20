e6de074ac338b82f91078dff21ba7270dc290ea1
jooijin <48429380+jooijin@users.noreply.github.com>
2022-11-20 20:43:17 +0900

    [22.11.20] 배포스크립트 오류 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -33,5 +33,5 @@ echo "> $JAR_NAME 실행"
 
 nohup java -jar \
        -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties,classpath:/application-real.properties \
-       -Dstring.profiles.active=real \
+       -Dspring.profiles.active=real \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
